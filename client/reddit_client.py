4ac47b1ba92d9fd544bd4c979696347dee0200fe
purvag03 <purvag@andrew.cmu.edu>
2023-12-11 21:47:48 -0500

    client

--- client/reddit_client.py
+++ client/reddit_client.py
@@ -17,7 +17,7 @@ class RedditClient:
         
 
     def create_post(self, title, text, media_url, author, subreddit):
-        # Create a request to create a new post
+        
         post_request = reddit_pb2.CreatePostRequest(
             title=title,
             text=text,
@@ -26,25 +26,20 @@ class RedditClient:
             subreddit=subreddit
         )
 
-        # Send the request to the server
         post_response = self.stub.CreatePost(post_request)
         return post_response
     
     def vote_post(self, post_id, upvote=True):
-        # Create a request to vote on a post
         vote_request = reddit_pb2.VotePostRequest(post_id=post_id, vote=upvote)
         try:
-            # Send the vote request to the server
             response = self.stub.VotePost(vote_request)
             print(f"Successfully {'upvoted' if upvote else 'downvoted'} Post ID {post_id}. New Score: {response.new_score}")
         except grpc.RpcError as e:
             print(f"RPC failed: {e.details()}")
             
     def get_post(self, post_id):
-        # Create a request to get a post by ID
         get_post_request = reddit_pb2.GetPostRequest(post_id=post_id)
         try:
-            # Send the request to the server
             response = self.stub.GetPost(get_post_request)
             if response.post:
                 return response.post
@@ -56,7 +51,6 @@ class RedditClient:
             return None
 
     def create_comment(self, post_id, content, author, parent_comment=None):
-        # Create a request to create a new comment
         comment_request = reddit_pb2.CreateCommentRequest(
             post_id=post_id,
             content=content,
@@ -65,7 +59,6 @@ class RedditClient:
         )
 
         try:
-            # Send the comment creation request to the server
             response = self.stub.CreateComment(comment_request)
             if response:
                 print(f"Comment created with ID: {response.comment.comment_id}")
@@ -80,20 +73,16 @@ class RedditClient:
             return None
 
     def vote_comment(self, comment_id, upvote=True):
-        # Create a request to vote on a comment
         vote_request = reddit_pb2.VoteCommentRequest(comment_id=comment_id, vote=upvote)
         try:
-            # Send the vote request to the server
             response = self.stub.VoteComment(vote_request)
             print(f"Successfully {'upvoted' if upvote else 'downvoted'} Comment ID {comment_id}. New Score: {response.new_score}")
         except grpc.RpcError as e:
             print(f"RPC failed: {e.details()}")
             
     def list_top_comments(self, post_id, number_of_comments):
-        # Create a request to get the top comments for a post
         request = reddit_pb2.TopCommentsRequest(post_id=post_id, number_of_comments=number_of_comments)
         try:
-            # Send the request to the server
             response = self.stub.TopComments(request)
             for comment in response.comments:
                 print(f"Comment ID: {comment.comment.comment_id}, Score: {comment.comment.score}, Has Replies: {'Yes' if comment.has_replies else 'No'}")
@@ -103,10 +92,8 @@ class RedditClient:
             return None
     
     def expand_comment_branch(self, comment_id, number_of_comments):
-        # Create a request to expand a comment branch
         request = reddit_pb2.ExpandCommentBranchRequest(comment_id=comment_id, number_of_comments=number_of_comments)
         try:
-            # Send the request to the server
             response = self.stub.ExpandCommentBranch(request)
             for comment_tree in response.comments:
                 self._print_comment_tree(comment_tree.comment, "  ", comment_tree.replies)
@@ -116,8 +103,9 @@ class RedditClient:
             return None
 
     def _print_comment_tree(self, comment, indent, replies):
-        # Print the comment tree with indentation
+        # Print the main comment
         print(f"{indent}Comment ID: {comment.comment_id}, Score: {comment.score}")
+        # Print each reply
         for reply in replies:
             self._print_comment_tree(reply, indent + "  ", reply.replies)
         
@@ -164,4 +152,41 @@ def main():
 
     comment_text = "This is a sample comment."
     comment_author = "user456"
-    comment_response = client.create_comment
+    comment_response = client.create_comment(post_id, comment_text, comment_author)
+    
+    if comment_response:
+        comment_id = comment_response.comment.comment_id
+        client.vote_comment(comment_id, upvote=True)
+        # client.vote_comment(comment_id, upvote=False)
+    
+         
+    reply_text = "This is a reply to the sample comment."
+    reply_author = "user789"
+    reply_response = client.create_comment(post_id, reply_text, reply_author, parent_comment=comment_id)
+    if reply_response:
+        print(f"\nReply created with ID: {reply_response.comment.comment_id}")
+        print(f"Reply: {reply_response.comment.content}")
+        print(f"Author: {reply_response.comment.author}")
+    else:
+        print("Failed to create reply")
+        
+    reply_text = "This is a reply to the sample comment2."
+    reply_author = "user789"
+    reply_response = client.create_comment(post_id, reply_text, reply_author, parent_comment=comment_id)
+    if reply_response:
+        print(f"\nReply created with ID: {reply_response.comment.comment_id}")
+        print(f"Reply: {reply_response.comment.content}")
+        print(f"Author: {reply_response.comment.author}")
+    else:
+        print("Failed to create reply")
+        
+    print()   
+        
+    top_comments = client.list_top_comments(post_id=post_id, number_of_comments=3)
+    
+    # Expanding the comment branch
+    print("Expanding comment branch:")
+    client.expand_comment_branch(comment_id, number_of_comments=3)
+
+if __name__ == "__main__":
+    main()
